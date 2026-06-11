6b8991151fbf6d46da271a645fd478b4b63513f8
Gilliard Costa Rodrigues <57216615@adp>
2026-06-11 13:51:08 -0300

    chore: remove arquivo de teste temporário

--- teste_fin.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect(r'C:\Users\57216615\sistema-gta-adepara\banco_gta.db')
-rows = conn.execute(
-    "SELECT DISTINCT json_extract(dados_json, '$.\"Finalidade do transporte\"') AS fin FROM gtas WHERE fin IS NOT NULL LIMIT 10"
-).fetchall()
-print(rows)
-conn.close()
